1c4c5fc65e336612bd6b559bfdf0ec141865b32f
chicm <idv2005@gmail.com>
2017-04-11 00:30:58 +0800

    updates

--- statefarm/statefarm.py
+++ statefarm/statefarm.py
@@ -1,6 +1,7 @@
+
+import os, shutil, glob, sys
 import pandas as pd
 import numpy as np
-import os, shutil, glob, sys
 from keras.preprocessing import image
 from keras.utils.np_utils import to_categorical
 from keras.models import Sequential, Model
@@ -13,7 +14,7 @@ from utils import *
 from vgg16 import *
 import argparse
 
-DATA_DIR = '/home/chicm/ml/deeplearningcourse/statefarm/data/sf'
+DATA_DIR = '/home/chicm/ml/cnnpractices/statefarm/data'
 TRAIN_DIR = DATA_DIR+'/train'
 TEST_DIR = DATA_DIR + '/test'
 VALID_DIR = DATA_DIR + '/valid'
@@ -32,18 +33,19 @@ def do_clip(arr, mx):
 
 def move_validation_back():
     os.chdir(VALID_DIR)
-    files = glob.glob('*/*.jpg')
+    files = glob('*/*.jpg')
     for fn in files:
         shutil.move(VALID_DIR+'/'+fn, TRAIN_DIR+'/'+fn)
 
 def create_validation_data():
-    drivers_ds = pd.read_csv('/home/chicm/ml/deeplearningcourse/statefarm/data/driver_imgs_list.csv')
+    drivers_ds = pd.read_csv(DATA_DIR + '/driver_imgs_list.csv')
     drivers_ds.head
     img2driver = drivers_ds.set_index('img')['subject'].to_dict()
     driver2imgs = {k: g["img"].tolist() 
                 for k, g in drivers_ds[['subject', 'img']].groupby("subject")}
 
-    drivers = sorted(driver2imgs.keys())
+    #drivers = sorted(driver2imgs.keys())
+    drivers = driver2imgs.keys()
     print drivers
 
     for i in range(4):
@@ -57,7 +59,7 @@ def get_vgg_model():
     model = vgg.model
     return model
 
-def gen_vgg_features(gen_train=True, gen_valid=True, gen_test=True):
+def gen_vgg_features(gen_train=False, gen_valid=False, gen_test=False):
     gen_t = image.ImageDataGenerator(rotation_range=15, height_shift_range=0.05,
 		shear_range=0.1, channel_shift_range=20, width_shift_range=0.1)
 
@@ -65,7 +67,7 @@ def gen_vgg_features(gen_train=True, gen_valid=True, gen_test=True):
     val_batches = get_batches(VALID_DIR, batch_size = batch_size, shuffle=False)
     test_batches = get_batches(TEST_DIR, batch_size = batch_size, shuffle=False)
 
-    model = get_vgg_model
+    model = get_vgg_model()
 
     last_conv_idx = [i for i,l in enumerate(model.layers) if type(l) is Convolution2D] [-1]
     conv_layers = model.layers[:last_conv_idx+1]
@@ -131,7 +133,7 @@ def save_predict():
     preds = bn_model.predict(conv_test_feat, batch_size=batch_size)
     save_array(PREDICTS_FILE, preds)
 
-def gen_submit(clip_percentage, submit_filename):
+def gen_submit(submit_filename, clip_percentage):
     preds = load_array(PREDICTS_FILE)
 
     subm = do_clip(preds, clip_percentage)
@@ -149,20 +151,43 @@ def gen_submit(clip_percentage, submit_filename):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--gentestfeats", action ='store_true', help="generate vgg conv layers output array for test data")
-parser.add_argument("--gentrainfeats", action ='store_true', help="generate vgg conv layers output array for train and validation data")
-parser.add_argument("--train", action ='store_true', help="train dense layers")
-parser.add_argument("--predict", action ='store_true', help="predict test data and save")
-parser.add_argument("--sub", nargs = 2, help="generate submission file")
+parser.add_argument("--mb", action='store_true', help="move validation data back to training directory")
+parser.add_argument("--createval", action='store_true',
+                    help="create validation data from training data")
+parser.add_argument("--gentestfeats", action='store_true',
+                    help="generate vgg conv layers output array for test data")
+parser.add_argument("--gentrainfeats", action='store_true',
+                    help="generate vgg conv layers output array for train and validation data")
+parser.add_argument("--train", action='store_true', help="train dense layers")
+parser.add_argument("--predict", action='store_true', help="predict test data and save")
+parser.add_argument("--sub", nargs=2, help="generate submission file")
 
 args = parser.parse_args()
+if args.mb:
+    print 'moving back...'
+    move_validation_back()
+    print 'done'
+if args.createval:
+    print 'creating val data...'
+    create_validation_data()
+    print 'done'
 if args.gentestfeats:
-    print args.gentestfeats
+    print 'generating test features...'
+    gen_vgg_features(gen_test=True)
+    print 'done'
 if args.gentrainfeats:
-    print args.gentrainfeats
+    print 'generating train and val features...'
+    gen_vgg_features(gen_train=True, gen_valid=True)
+    print 'done'
 if args.train:
-    print args.train
+    print 'training dense layer...'
+    train_bn_layers()
+    print 'done'
 if args.predict:
-    print args.predict
+    print 'predicting test data...'
+    save_predict()
+    print 'done'
 if args.sub:
-    print args.sub
+    print 'generating submision file...'
+    gen_submit(args.sub[0], (float)(args.sub[1]))
+    print 'done'
